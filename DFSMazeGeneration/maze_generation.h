ca6f939d6a32586f900592d21748c31b01895c3e
romandykyi <roman.dykyi.8210@gmail.com>
2023-12-01 22:25:46 +0100

    Remove commented code

--- DFSMazeGeneration/maze_generation.h
+++ DFSMazeGeneration/maze_generation.h
@@ -6,5 +6,3 @@
 void generate_maze_binary_tree(uint8_t* const maze, const uint32_t width, const uint32_t height);
 
 void generate_maze_sidewinder(uint8_t* const maze, const uint32_t width, const uint32_t height);
-
-void generate_maze_dfs(uint8_t* maze, const uint32_t width, const uint32_t height);
